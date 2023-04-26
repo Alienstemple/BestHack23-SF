003689657e55fd6188a4038504002de0b0291027
Alyona <borisovsk_a@mail.ru>
2023-04-26 07:59:21 +0300

    feat: Add uploadFile, FileRepository

--- app/src/main/java/com/example/besthack23/MainActivity.kt
+++ app/src/main/java/com/example/besthack23/MainActivity.kt
@@ -2,10 +2,34 @@ package com.example.besthack23
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import com.example.besthack23.databinding.ActivityMainBinding
+import java.io.File
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+    private val fileRepository = FileRepository()
+    private lateinit var fileAsString: String  // Строка, с которой можно работать
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        setUploadFileListener()
+    }
+
+    private fun setUploadFileListener() {
+        binding.uploadBtn.setOnClickListener {
+            try {
+                fileAsString = fileRepository.uploadAndReadFile()
+            } catch (e: Exception) {
+                Log.e(TAG, "Cannot open file! ${e.stackTrace}")
+            }
+        }
+    }
+
+    companion object {
+        const val TAG = "MainActLog"
     }
 }
