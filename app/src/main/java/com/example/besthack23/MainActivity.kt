b3b074a1a38fa9d6680f0e4b9614dd97a4daa3e6
Alyona <borisovsk_a@mail.ru>
2023-04-27 19:47:51 +0300

    feat: Read text plain working

--- app/src/main/java/com/example/besthack23/MainActivity.kt
+++ app/src/main/java/com/example/besthack23/MainActivity.kt
@@ -4,18 +4,16 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import com.example.besthack23.databinding.ActivityMainBinding
-import java.io.File
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
-    private val fileRepository = FileRepository()
+    private val fileRepository = FileRepository(this)
     private lateinit var fileAsString: String  // Строка, с которой можно работать
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
-
         setUploadFileListener()
     }
 
