63a164d6dacce00dced84128e5f4e972715303ed
Alyona <borisovsk_a@mail.ru>
2023-04-27 21:16:34 +0300

    debug: Read file as Flow maybe?

--- app/src/main/java/com/example/besthack23/MainActivity.kt
+++ app/src/main/java/com/example/besthack23/MainActivity.kt
@@ -3,29 +3,72 @@ package com.example.besthack23
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import androidx.activity.result.ActivityResultLauncher
+import androidx.activity.result.contract.ActivityResultContracts
 import com.example.besthack23.databinding.ActivityMainBinding
+import kotlinx.coroutines.GlobalScope.coroutineContext
+import kotlinx.coroutines.channels.trySendBlocking
+import kotlinx.coroutines.coroutineScope
+import kotlinx.coroutines.flow.callbackFlow
+import kotlinx.coroutines.flow.collect
+import kotlinx.coroutines.runBlocking
+import kotlin.coroutines.coroutineContext
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
-    private val fileRepository = FileRepository(this)
-    private lateinit var fileAsString: String  // Строка, с которой можно работать
+    private var fileAsString: String? = null  // Строка, с которой можно работать
+
+    private lateinit var openLauncher: ActivityResultLauncher<Array<String>>
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        registerOpenLauncher()
         setUploadFileListener()
     }
 
-    private fun setUploadFileListener() {
-        binding.uploadBtn.setOnClickListener {
+    private fun registerOpenLauncher() {
+        openLauncher = registerForActivityResult(ActivityResultContracts.OpenDocument()) { uri ->
             try {
-                fileAsString = fileRepository.uploadAndReadFile()
+                uri?.let {
+                    fileAsString = contentResolver.openInputStream(uri)?.use {
+                        String(it.readBytes())
+                    } ?: throw IllegalStateException("Can't open input stream")
+                }
             } catch (e: Exception) {
                 Log.e(TAG, "Cannot open file! ${e.stackTrace}")
             }
         }
     }
+
+    private fun setUploadFileListener() {
+        binding.uploadBtn.setOnClickListener {
+            runBlocking {
+                openLauncher.launch(arrayOf("text/plain", "text/fb2+xml", "application/epub+zip"))
+                Log.d(TAG, "File is: $fileAsString")
+            }
+        }
+    }
+
+//    private fun open() = callbackFlow<String> {
+//
+//        val openLauncher: ActivityResultLauncher<Array<String>> =
+//            registerForActivityResult(ActivityResultContracts.OpenDocument()) { uri ->
+//                try {
+//                    uri?.let {
+//                        val resString = contentResolver.openInputStream(uri)?.use {
+//                            String(it.readBytes())
+//                        } ?: throw IllegalStateException("Can't open input stream")
+//                        trySendBlocking(resString)
+//                    }
+//                } catch (e: Exception) {
+//                    Log.e(TAG, "Cannot open file! ${e.stackTrace}")
+//                }
+//            }
+//
+//        openLauncher.launch(arrayOf("text/plain", "text/fb2+xml", "application/epub+zip"))
+//    }
 
     companion object {
         const val TAG = "MainActLog"
